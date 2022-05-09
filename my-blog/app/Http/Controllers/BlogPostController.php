6680c6b1315a66731fce909343f31d0ef7956ff4
evdesign <eliyaou5423703@gmail.com>
2022-05-09 09:32:36 +0300

    added views

--- my-blog/app/Http/Controllers/BlogPostController.php
+++ my-blog/app/Http/Controllers/BlogPostController.php
@@ -15,7 +15,9 @@ class BlogPostController extends Controller
     public function index()
     {
         $posts = BlogPost::all(); //fetch all blog posts from DB
-        return $posts; //returns the fetched posts
+        return view('blog.index', [
+                'posts' => $posts,
+            ]); //returns the view with posts
     }
 
     /**
@@ -25,7 +27,7 @@ class BlogPostController extends Controller
      */
     public function create()
     {
-        //
+        return view('blog.create');
     }
 
     /**
@@ -36,7 +38,13 @@ class BlogPostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $newPost = BlogPost::create([
+            'title' => $request->title,
+            'body' => $request->body,
+            'user_id' => 1
+        ]);
+
+        return redirect('blog/' . $newPost->id);
     }
 
     /**
@@ -47,7 +55,7 @@ class BlogPostController extends Controller
      */
     public function show(BlogPost $blogPost)
     {
-        return $blogPost;
+        return view('blog.show', ["post" => $blogPost]);
     }
 
     /**
@@ -58,7 +66,9 @@ class BlogPostController extends Controller
      */
     public function edit(BlogPost $blogPost)
     {
-        //
+        return view("blog.edit", [
+            "post" => $blogPost
+            ]); //returns the edit view with the post
     }
 
     /**
@@ -70,7 +80,12 @@ class BlogPostController extends Controller
      */
     public function update(Request $request, BlogPost $blogPost)
     {
-        //
+        $blogPost->update([
+            'title' => $request->title,
+            'body' => $request->body
+        ]);
+
+        return redirect('blog/' . $blogPost->id);
     }
 
     /**
@@ -81,6 +96,8 @@ class BlogPostController extends Controller
      */
     public function destroy(BlogPost $blogPost)
     {
-        //
+        $blogPost->delete();
+
+        return redirect('/blog');
     }
 }
